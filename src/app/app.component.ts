124364538332ece87c2347bad2887671603c6215
vivianmartinez <128734243+vivianmartinez@users.noreply.github.com>
2023-10-04 21:31:40 +0200

    modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,6 +39,6 @@ export class AppComponent implements OnInit{
   }
 
   downloadFile() {
-    DownloadService.download('/assets/pdf/','cv-vivian-martinez.pdf','cv-vivianmartinez');
+    DownloadService.download('/assets/pdf/','cv-vivian-martinez.pdf');
   }
 }
